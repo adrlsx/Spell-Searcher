3accbf5943bd0b8f8b14b2b75c8a4ab6bf78320f
maximenrb <maxnarbaud@gmail.com>
2020-12-15 23:41:42 +0100

    Put creature details frame in SpellFrame.scala

--- src/main/scala/SpellFrame.scala
+++ src/main/scala/SpellFrame.scala
@@ -1,9 +1,12 @@
 import SwingGeneralFunc.{addSeparator, getHorizontalBox, getWebsiteBtnBox}
 import org.apache.spark.sql.{Column, DataFrame, Row}
 
-import java.awt.Image
+import java.awt.{Desktop, Image}
 import java.awt.event.{MouseAdapter, MouseEvent}
+import java.net.URI
 import javax.swing._
+import javax.swing.text.html.StyleSheet.BoxPainter
+import scala.swing
 import scala.swing._
 
 class SpellFrame(searcher: Searcher.type, sparkRequest: SparkRequest.type, spellName: String) extends Frame {
@@ -48,84 +51,150 @@ class SpellFrame(searcher: Searcher.type, sparkRequest: SparkRequest.type, spell
   val jPanelResult = new JPanel()
   jPanelResult.setLayout(new BoxLayout(jPanelResult, BoxLayout.Y_AXIS))
 
-  contents = new BoxPanel(Orientation.Vertical) {
+  // initialisation for creature frame
+  val creatureNameLabel: Label = new Label()
+  val creatureSpellLabel: Label = new Label()
+
+  val creatureWebSiteBtn: Button = Button("") {}
+  creatureWebSiteBtn.background = new Color(52, 152, 219)
+
+  val descriptionTextArea = new JTextArea()
+  descriptionTextArea.setEditable(false)
+  descriptionTextArea.setLineWrap(true)
+  descriptionTextArea.setWrapStyleWord(true)
+
+  val creatureBoxPanel: BoxPanel = new BoxPanel(Orientation.Horizontal) {
+    contents += Swing.HStrut(10)
+    contents += Component.wrap(new JSeparator(SwingConstants.VERTICAL))
+    contents += Swing.HStrut(10)
+
+    contents += new BoxPanel(Orientation.Vertical) {
+      // Add creature web page button
+      contents += new BoxPanel(Orientation.Horizontal) {
+        contents += creatureWebSiteBtn
+        contents += Swing.HGlue
+      }
+
+      contents += Swing.VStrut(5)
+
+      // Add creature name box
+      contents += new BoxPanel(Orientation.Horizontal) {
+        contents += new Label("Creature Name: ")
+        contents += creatureNameLabel
+        contents += Swing.HGlue
+      }
+
+      contents += Swing.VStrut(5)
+
+      // Add creature spells box
+      contents += new BoxPanel(Orientation.Horizontal) {
+        contents += new Label("Spell: ")
+        contents += creatureSpellLabel
+        contents += Swing.HGlue
+      }
+
+      contents += Swing.VStrut(5)
+
+      // Add creature description box
+      contents += getHorizontalBox("Description", "")
+      contents += Component.wrap(descriptionTextArea)
+
+      contents += Swing.Glue
+    }
+  }
+
+  contents = new BoxPanel(Orientation.Horizontal) {
     // Set frame border margin
     border = Swing.EmptyBorder(10, 10, 10, 10)
 
-    contents += new BoxPanel(Orientation.Horizontal) {
-      // Add spell picture
-      contents += new Label { icon = imageIcon }
+    contents += new BoxPanel(Orientation.Vertical) {
+      contents += new BoxPanel(Orientation.Horizontal) {
+        // Add spell picture
+        contents += new Label { icon = imageIcon }
 
-      contents += Swing.HStrut(10)
+        contents += Swing.HStrut(10)
 
-      contents += new BoxPanel(Orientation.Vertical) {
+        contents += new BoxPanel(Orientation.Vertical) {
 
-        contents += new BoxPanel(Orientation.Horizontal) {
-          contents += new Label("Creature Result: ")
-          contents += Swing.HGlue
-          contents += labelNbResult
+          contents += new BoxPanel(Orientation.Horizontal) {
+            contents += new Label("Creature Result: ")
+            contents += Swing.HGlue
+            contents += labelNbResult
+          }
+
+          contents += Swing.VStrut(5)
+
+          val jScrollPaneResult = new JScrollPane(jPanelResult)
+          jScrollPaneResult.setPreferredSize(new Dimension(300, 583))
+
+          // https://stackoverflow.com/questions/5583495/how-do-i-speed-up-the-scroll-speed-in-a-jscrollpane-when-using-the-mouse-wheel
+          jScrollPaneResult.getVerticalScrollBar.setUnitIncrement(16)
+
+          // Use Component.wrap() for link between javax.swing and scala.swing
+          contents += Component.wrap(jScrollPaneResult)
         }
-
-        contents += Swing.VStrut(5)
-
-        val jScrollPaneResult = new JScrollPane(jPanelResult)
-        jScrollPaneResult.setPreferredSize(new Dimension(300, 583))
-
-        // https://stackoverflow.com/questions/5583495/how-do-i-speed-up-the-scroll-speed-in-a-jscrollpane-when-using-the-mouse-wheel
-        jScrollPaneResult.getVerticalScrollBar.setUnitIncrement(16)
-
-        // Use Component.wrap() for link between javax.swing and scala.swing
-        contents += Component.wrap(jScrollPaneResult)
       }
+
+      addSeparator(contents)
+
+      // Add spell web page button
+      contents += getWebsiteBtnBox(spellUrl)
+      contents += Swing.VStrut(5)
+
+      // Add spell name box
+      contents += getHorizontalBox("Spell name", spellName)
+      contents += Swing.VStrut(5)
+
+      // Add spell schools box
+      contents += getHorizontalBox("School", spellSchool)
+      contents += Swing.VStrut(5)
+
+      // Add spell classes box
+      contents += getHorizontalBox("Classes", spellClasses)
+      contents += Swing.VStrut(5)
+
+      // Add spell components box
+      contents += getHorizontalBox("Components", spellComponents)
+      contents += Swing.VStrut(5)
+
+      // Add spell resistance box
+      contents += getHorizontalBox("Spell Resistance", spellResistance)
     }
 
-    addSeparator(contents)
+    creatureBoxPanel.preferredSize = new Dimension(350, 0)
+    creatureBoxPanel.visible = false
 
-    // Add spell web page button
-    contents += getWebsiteBtnBox(spellUrl)
-    contents += Swing.VStrut(5)
+    contents += creatureBoxPanel
+  }
 
-    // Add spell name box
-    contents += getHorizontalBox("Name", spellName)
-    contents += Swing.VStrut(5)
+  def showCreatureFrame(selectedCreatureName: String): Unit = {
+    sparkRequest.getCreatureInfo(selectedCreatureName)
 
-    // Add spell schools box
-    contents += getHorizontalBox("School", spellSchool)
-    contents += Swing.VStrut(5)
+    creatureNameLabel.text = selectedCreatureName
+    creatureSpellLabel.text = "Spells of " + selectedCreatureName
 
-    // Add spell classes box
-    contents += getHorizontalBox("Classes", spellClasses)
-    contents += Swing.VStrut(5)
+    creatureWebSiteBtn.action = swing.Action("See creature on Archives of Nethys") {
+      Desktop.getDesktop.browse(new URI("https://aonprd.com/MonsterDisplay.aspx?ItemName=" + selectedCreatureName))
+    }
 
-    // Add spell components box
-    contents += getHorizontalBox("Components", spellComponents)
-    contents += Swing.VStrut(5)
+    descriptionTextArea.setText("Long description qui ne tient pas sur une seule ligne sinon c'est pas drôle de " + selectedCreatureName)
 
-    // Add spell resistance box
-    contents += getHorizontalBox("Spell Resistance", spellResistance)
+    creatureBoxPanel.visible = true
+
+    // Resize window after set creature box visible
+    this.pack()
+    this.centerOnScreen()
   }
 
   addCreatureResult()
 
   def addCreatureResult(): Unit = {
-    for (_ <- 0 until 50) {
-      val jLabel = new JLabel("TEST")
+    for (i <- 0 until 50) {
+      val jLabel = new JLabel("TEST" + i)
 
       jLabel.addMouseListener(new MouseAdapter() {
-        // https://stackoverflow.com/questions/2440134/is-this-the-proper-way-to-initialize-null-references-in-scala
-        // https://alvinalexander.com/scala/initialize-scala-variables-option-none-some-null-idiom/
-        private var creatureDisplay = Option.empty[CreatureFrame]
         override def mouseClicked(e: MouseEvent) {
-          // Create the SpellFrame if it has not been created before
-          if (creatureDisplay.isEmpty){
-            creatureDisplay = Some(new CreatureFrame(searcher, sparkRequest, jLabel.getText))
-            creatureDisplay.get.centerOnScreen()
-            creatureDisplay.get.open()
-          }
-          // elsewhere simply bring the window to the front
-          else{
-            creatureDisplay.get.open()
-          }
+          showCreatureFrame(jLabel.getText)
         }
       })
 
