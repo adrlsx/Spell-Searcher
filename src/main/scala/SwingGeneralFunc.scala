bd9836945a31e6b4bb8a42621ed568e4f71b3e0c
maximenrb <maxnarbaud@gmail.com>
2020-12-19 22:30:21 +0100

    Edit text for website button

--- src/main/scala/SwingGeneralFunc.scala
+++ src/main/scala/SwingGeneralFunc.scala
@@ -21,7 +21,7 @@ object SwingGeneralFunc {
 
   def getWebsiteBtnBox(url: String): BoxPanel = {
     new BoxPanel(Orientation.Horizontal) {
-      val websiteBtn: Button = Button("See on Archives of Nethys") { Desktop.getDesktop.browse(new URI(url)) }
+      val websiteBtn: Button = Button("See spell on Archives of Nethys") { Desktop.getDesktop.browse(new URI(url)) }
       websiteBtn.background = new Color(52, 152, 219)
 
       contents += websiteBtn
